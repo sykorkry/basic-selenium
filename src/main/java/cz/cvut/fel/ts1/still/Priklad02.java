fffb1904bb29b88579ad14b22b367fd3f0b29fb8
Krystof <krystof@krystof-desktop>
2020-04-28 13:27:39 +0200

    Updates Priklad02

--- src/main/java/cz/cvut/fel/ts1/still/Priklad02.java
+++ src/main/java/cz/cvut/fel/ts1/still/Priklad02.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class Priklad02 {
 
@@ -16,7 +19,20 @@ public class Priklad02 {
             driver.get("http://35.246.223.13/");
             driver.findElement(By.id("firstName")).sendKeys("Honza");
             driver.findElement(By.id("lastName")).sendKeys("Vocásek");
-//            System.out.println(e.getAttribute("textContent"));
+            driver.findElement(By.id("email")).sendKeys("honzavocaek@gmail.com");
+
+            driver.findElement(By.id("birthDate")).sendKeys("2017-10-01");
+
+            Select selectDestination = new Select(driver.findElement(By.id("destination")));
+            selectDestination.selectByVisibleText("London");
+
+            driver.findElement(By.id("student")).click();
+            driver.findElement(By.id("bank_transfer")).click();
+
+            driver.findElement(By.cssSelector("body > form > div > button")).click();
+
+            WebElement e = driver.findElement(By.cssSelector("body > h2:nth-child(5)"));
+            System.out.println(e.getAttribute("textContent"));
         } finally {
             driver.quit();
         }
